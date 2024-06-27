99a6c9f968d9f9dc569dfcc81bf56534666e9d06
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2024-06-27 13:53:06 -0300

    criar script com as classes veiculo e carro

---
+++ js/classes.js
@@ -0,0 +1,45 @@
+class Veiculo {
+  constructor(
+    marca,
+    modelo,
+    anoFabricacao,
+    cor,
+    tipo,
+    kilometragem,
+    numeroPortas,
+    preco
+  ) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anoFabricacao = anoFabricacao;
+    this.cor = cor;
+    this.tipo = tipo;
+    this.kilometragem = kilometragem;
+    this.numeroPortas = numeroPortas;
+    this.preco = preco;
+  }
+}
+
+class Carro extends Veiculo {
+  constructor(
+    marca,
+    modelo,
+    anoFabricacao,
+    cor,
+    tipo,
+    kilometragem,
+    numeroPortas,
+    preco
+  ) {
+    super(
+      marca,
+      modelo,
+      anoFabricacao,
+      cor,
+      tipo,
+      kilometragem,
+      numeroPortas,
+      preco
+    );
+  }
+}
