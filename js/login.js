5b9e4ec0d29122f6ea3ec7bf1467ca65e7c4429c
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2024-06-27 11:26:44 -0300

    criar script para fazer login do usuario

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// Função para verificar se login e senha estão corretos
+function login(user, password) {
+  return user === "admin" && password === "admin";
+}
+
+// Acessar elemento de formulário de login
+const loginForm = document.getElementById("login-form");
+
+// Adicionar listener para evento de submit do formulário
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const user = formData.get("user");
+  const password = formData.get("password");
+
+  if (login(user, password)) {
+    window.location.href = "./index.html";
+  } else {
+    alert("Usuário ou Senha incorretos!");
+  }
+});
