d89e36f23d739c0985128f206bd719df6e8c01a4
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2024-06-28 20:49:52 -0300

    criar pagina de cadastrar e funcionalidade de carregar e salvar carros cadastrados

---
+++ js/cadastrar.js
@@ -0,0 +1,37 @@
+// Acessar elemento de formulário de cadastrar carros
+const formCadastrar = document.getElementById("form-cadastrar");
+
+// Adicionar listener para evento de submit do formulário
+formCadastrar.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(formCadastrar);
+  const marca = formData.get("marca");
+  const modelo = formData.get("modelo");
+  const anoFabricacao = formData.get("anoFabricacao");
+  const cor = formData.get("cor");
+  const tipo = formData.get("tipo");
+  const kilometragem = formData.get("kilometragem");
+  const numeroPortas = formData.get("numeroPortas");
+  const preco = formData.get("preco");
+
+  const novoCarro = new Carro(
+    marca,
+    modelo,
+    anoFabricacao,
+    cor,
+    tipo,
+    kilometragem,
+    numeroPortas,
+    preco
+  );
+
+  const carros = carregaCarros();
+
+  if (carros.length > 0 && carros.some((carro) => carro.isSame(novoCarro))) {
+    alert("O carro não pode ser cadastrado pois já existe!");
+  } else {
+    carros.push(novoCarro);
+    salvarCarros(carros);
+  }
+});
--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,7 @@
+// Constante com o valor da chave do local storage dos carros salvos
+const LOCAL_CARROS_KEY = "carros";
+
+// Classe veiculo
 class Veiculo {
   constructor(
     marca,
@@ -18,8 +22,54 @@ class Veiculo {
     this.numeroPortas = numeroPortas;
     this.preco = preco;
   }
+
+  isSameMarca(carro) {
+    return this.marca === carro.marca;
+  }
+
+  isSameModelo(carro) {
+    return this.modelo === carro.modelo;
+  }
+
+  isSameAno(carro) {
+    return this.anoFabricacao === carro.anoFabricacao;
+  }
+
+  isSameCor(carro) {
+    return this.cor === carro.cor;
+  }
+
+  isSameTipo(carro) {
+    return this.tipo === carro.tipo;
+  }
+
+  isSameKm(carro) {
+    return this.kilometragem === carro.kilometragem;
+  }
+
+  isSamePortas(carro) {
+    return this.numeroPortas === carro.numeroPortas;
+  }
+
+  isSamePreco(carro) {
+    return this.preco === carro.preco;
+  }
+
+  isSame(carro) {
+    return (
+      this.isSameMarca(carro) &&
+      this.isSameModelo(carro) &&
+      this.isSameAno(carro) &&
+      this.isSameCor(carro) &&
+      this.isSameTipo(carro) &&
+      this.isSameKm(carro) &&
+      this.isSamePortas(carro) &&
+      this.isSamePreco(carro)
+    );
+  }
 }
 
+// Classe carro que herda a classe veiculo
 class Carro extends Veiculo {
   constructor(
     marca,
@@ -42,4 +92,37 @@ class Carro extends Veiculo {
       preco
     );
   }
+}
+
+// Função para carregar os carros que estão salvos no localStorage
+function carregaCarros() {
+  const localCarros = localStorage.getItem(LOCAL_CARROS_KEY);
+  let parsedLocalCarros = [];
+  const carros = [];
+
+  if (localCarros) {
+    parsedLocalCarros = JSON.parse(localCarros);
+  }
+
+  parsedLocalCarros.forEach((carro) => {
+    carros.push(
+      new Carro(
+        carro.marca,
+        carro.modelo,
+        carro.anoFabricacao,
+        carro.cor,
+        carro.tipo,
+        carro.kilometragem,
+        carro.numeroPortas,
+        carro.preco
+      )
+    );
+  });
+
+  return carros;
+}
+
+// Função para salvar os carros no localStorage
+function salvarCarros(carros) {
+  localStorage.setItem(LOCAL_CARROS_KEY, JSON.stringify(carros));
 }
